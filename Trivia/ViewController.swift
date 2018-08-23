10a76d379d8beeec63d57900e58e4fd59ce07105
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-08-23 16:21:38 -0400

    Added label that outputs questions

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -18,6 +18,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputQuestions: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
